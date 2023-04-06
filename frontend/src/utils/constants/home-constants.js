a11b2044445112cd9e6b85d819dc4381f5096a5c
Tyler Hum <tyler.hum@gmail.com>
2023-04-06 11:19:27 -0600

    Styles: Home
    Styles the hero banner for home page. Adds dropshadow, textshadow, font changes, social media icon buttons, and responsive spacing for text and image

--- frontend/src/utils/constants/home-constants.js
+++ frontend/src/utils/constants/home-constants.js
@@ -1,28 +1,12 @@
 const HomeItems = {
-    herobanner: {
-        itemOne: {
-
-        },
-        itemTwo: {
-
-        },
-        itemThree: {
-
-        },
+    profile: {
+        img: '/img/tyler-hum.webp',
+        alt: 'An profile image of Tyler Hum, a male in his thirties with black square-lensed glasses. A rich blue background is behind him.',
+        title1: 'Full-Stack',
+        title2: 'Application Developer',
+        text: `Hi! I'm Tyler Hum. A passionate web applications developer based in `,
+        textWidow: 'Edmonton, Alberta.',
     },
-    pathblocks: {
-        itemOne: {
+};
 
-        },
-        itemTwo: {
-
-        },
-        itemThree: {
-
-        },
-    },
-    card: {
-
-    }
-
-}
+export { HomeItems };
