c369ef0102ed7f288a892242a5fb66dfaffc4865
Tyler Hum <tyler.hum@gmail.com>
2023-04-07 15:04:42 -0600

    Feat: Adds tech stack items to consts

--- frontend/src/utils/constants/home-constants.js
+++ frontend/src/utils/constants/home-constants.js
@@ -4,9 +4,78 @@ const HomeItems = {
         alt: 'An profile image of Tyler Hum, a male in his thirties with black square-lensed glasses. A rich blue background is behind him.',
         title1: 'Full-Stack',
         title2: 'Application Developer',
-        text: `Hi! I'm Tyler Hum. A passionate web applications developer based in `,
+        text: `Hi! My name is Tyler. I'm a passionate creative with a penchant for storytelling, building websites, and developing full-stack web-based applications. You'll find me in beautiful `,
         textWidow: 'Edmonton, Alberta.',
     },
 };
 
-export { HomeItems };
+const FrontEndStack = [
+    {
+        id: 1,
+        name: 'HTML5',
+        img: '../svg/html-5.svg',
+        alt: 'An image of the HTML 5 logo.',
+        url: 'https://html.com/html5/',
+    },
+    {
+        id: 2,
+        name: 'CSS',
+        img: '../svg/css-3.svg',
+        alt: 'An image of the CSS 3 logo.',
+        url: 'https://www.w3.org/Style/CSS/Overview.en.html',
+    },
+    {
+        id: 3,
+        name: 'JavaScript',
+        img: '../svg/js.svg',
+        alt: 'An image of the JavaScript logo.',
+        url: 'https://www.javascript.com/',
+    },
+    {
+        id: 4,
+        name: 'React',
+        img: '../svg/react.svg',
+        alt: 'An image of the React logo.',
+        url: 'https://react.dev/',
+    },
+    {
+        id: 5,
+        name: 'Next.js',
+        img: '../svg/nextjs.svg',
+        alt: 'An image of the Next.js logo.',
+        url: 'https://nextjs.org/',
+    },
+];
+
+const BackEndStack = [
+    {
+        id: 1,
+        name: 'C#',
+        img: '../svg/csharp.svg',
+        alt: 'An image of a C# logo.',
+        url: 'https://learn.microsoft.com/en-us/dotnet/csharp/',
+    },
+    {
+        id: 2,
+        name: 'ASP.NET Core',
+        img: '../svg/dotnetcore.svg',
+        alt: 'An image of the .Net Core logo.',
+        url: 'https://learn.microsoft.com/en-us/aspnet/core/introduction-to-aspnet-core?view=aspnetcore-7.0',
+    },
+    {
+        id: 3,
+        name: 'Java & Jakarta',
+        img: '../svg/java.svg',
+        alt: 'An image of the Java logo.',
+        url: 'https://www.oracle.com/ca-en/java/',
+    },
+    {
+        id: 4,
+        name: 'MS SQL Server',
+        img: '../svg/sql.svg',
+        alt: 'An image of the SQL logo.',
+        url: 'https://www.microsoft.com/en-ca/sql-server/sql-server-2019',
+    },
+];
+
+export { HomeItems, FrontEndStack, BackEndStack };
