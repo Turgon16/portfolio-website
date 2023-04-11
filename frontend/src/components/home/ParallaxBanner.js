b49e3121d001900ae1b145debcc1a2f6761522dd
Tyler Hum <tyler.hum@gmail.com>
2023-04-11 10:02:09 -0600

    Styles: Adds drawers and styling for parallax

--- frontend/src/components/home/ParallaxBanner.js
+++ frontend/src/components/home/ParallaxBanner.js
@@ -1,97 +1,272 @@
 // Next & React Imports
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 // MUI Imports
+import CloseIcon from '@mui/icons-material/Close';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 // Project & Style Imports
-import { BackEndStack, FrontEndStack } from '@/utils/constants/home-constants';
+import {
+    BackEndStack,
+    FrontEndBox,
+    FrontEndStack,
+} from '@/utils/constants/home-constants';
+import { IconButton } from '@mui/material';
 import styles from '../../styles/Home.module.css';
 
 const ParallaxBanner = () => {
+    const [showClass1, setShowClass1] = useState('false');
+    const [showClass2, setShowClass2] = useState('false');
     const router = useRouter();
 
-    return (
-        <Box className={styles.parallaxBanner}>
-            <Paper className={styles.parallaxContainer1}>
-                <Typography className={styles.parallaxTitle}>
-                    Front-end
-                </Typography>
-                <List>
-                    {FrontEndStack.map((tech, index) => (
-                        <ListItem>
-                            <ListItemButton
-                                className={styles.parallaxListButton}
-                                component="a"
-                                href={tech.url}
-                                target="_blank"
-                            >
-                                <ListItemIcon>
-                                    <Box
-                                        component={'img'}
-                                        className={styles.parallaxIcon}
-                                        alt={tech.alt}
-                                        src={tech.img}
-                                    />
-                                </ListItemIcon>
-                                <Typography className={styles.parallaxText}>
-                                    {tech.name}
-                                </Typography>
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-            </Paper>
+    const handleClass1Toggle = () => {
+        setShowClass1(!showClass1);
+    };
 
-            <Paper className={styles.parallaxContainer2}>
-                <Typography className={styles.parallaxTitle}>
-                    Back-end
-                </Typography>
-                <List>
-                    {BackEndStack.map((tech, index) => (
+    const handleClass2Toggle = () => {
+        setShowClass2(!showClass2);
+    };
+
+    return (
+        <Box id={'front-end'} className={styles.parallaxBanner}>
+            <Box className={styles.parallaxTop}>
+                <Box className={styles.parallaxContainer1}>
+                    <Typography className={styles.parallaxTitle}>
+                        Front-end
+                    </Typography>
+                    <List>
+                        {FrontEndStack.map((tech, index) => (
+                            <ListItem key={index}>
+                                <Box className={styles.parallaxListBox}>
+                                    <ListItemIcon>
+                                        <Box
+                                            component={'img'}
+                                            className={styles.parallaxIcon}
+                                            alt={tech.alt}
+                                            src={tech.img}
+                                        />
+                                    </ListItemIcon>
+                                    <Typography className={styles.parallaxText}>
+                                        {tech.name}
+                                    </Typography>
+                                </Box>
+                            </ListItem>
+                        ))}
                         <ListItem>
                             <ListItemButton
-                                className={styles.parallaxListButton}
-                                component="a"
-                                href={tech.url}
-                                target="_blank"
+                                className={styles.parallaxButton}
+                                onClick={handleClass1Toggle}
+                                href={'#front-end'}
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                }}
                             >
-                                <ListItemIcon>
-                                    <Box
-                                        component={'img'}
-                                        className={styles.parallaxIcon}
-                                        alt={tech.alt}
-                                        src={tech.img}
-                                    />
-                                </ListItemIcon>
-                                <Typography className={styles.parallaxText}>
-                                    {tech.name}
+                                <Typography className={styles.buttonText}>
+                                    Learn More
                                 </Typography>
+                                <KeyboardDoubleArrowRightIcon fontSize="large" />
                             </ListItemButton>
                         </ListItem>
-                    ))}
-                    <ListItem>
-                        <ListItemButton
-                            className={styles.button}
-                            component="a"
-                            href="#"
-                            sx={{ display: 'flex', justifyContent: 'center' }}
+                    </List>
+                </Box>
+                <Box
+                    className={
+                        showClass1
+                            ? styles.parallaxContainer3
+                            : styles.parallaxContainer3 +
+                              ' ' +
+                              styles.showContainer3
+                    }
+                >
+                    <IconButton
+                        onClick={handleClass1Toggle}
+                        href={'#front-end'}
+                    >
+                        <Typography
+                            sx={{ color: 'white', whiteSpace: 'nowrap' }}
                         >
-                            <Typography className={styles.buttonText}>
-                                Learn More
-                            </Typography>
-                            <KeyboardDoubleArrowRightIcon fontSize="large" />
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-            </Paper>
+                            Close Window
+                        </Typography>
+                        <CloseIcon className={styles.parallaxCloseIcon} />
+                    </IconButton>
+                    <Box
+                        component={'img'}
+                        className={styles.parallaxImg}
+                        alt={FrontEndBox.alt1}
+                        src={FrontEndBox.img1}
+                    />
+                    <Typography className={styles.parallaxDrawerText}>
+                        {FrontEndBox.text1}
+                        <span className={'widow-wrap'}>
+                            {FrontEndBox.text1Widow}
+                        </span>
+                    </Typography>
+                    <Box className={'flex-evenly'}>
+                        <Box
+                            component={'img'}
+                            className={styles.parallaxImg2}
+                            alt={FrontEndBox.alt3}
+                            src={FrontEndBox.img3}
+                        />
+                    </Box>
+                    <Typography className={styles.parallaxDrawerText}>
+                        {FrontEndBox.text2}
+                        <span className={'widow-wrap'}>
+                            {FrontEndBox.text2Widow}
+                        </span>
+                    </Typography>
+                    <Box
+                        component={'img'}
+                        className={styles.parallaxImg}
+                        alt={FrontEndBox.alt2}
+                        src={FrontEndBox.img2}
+                    />
+                    <Typography className={styles.parallaxDrawerText}>
+                        {FrontEndBox.text3}
+                        <span className={'widow-wrap'}>
+                            {FrontEndBox.text3Widow}
+                        </span>
+                    </Typography>
+                    <IconButton
+                        onClick={handleClass1Toggle}
+                        href={'#front-end'}
+                    >
+                        <Typography
+                            sx={{ color: 'white', whiteSpace: 'nowrap' }}
+                        >
+                            Close Window
+                        </Typography>
+                        <CloseIcon
+                            className={styles.parallaxCloseIcon}
+                            href={'#'}
+                        />
+                    </IconButton>
+                </Box>
+            </Box>
+
+            <Box className={styles.parallaxBottom}>
+                <Box className={styles.parallaxContainer2}>
+                    <Typography className={styles.parallaxTitle}>
+                        Back-end
+                    </Typography>
+                    <List>
+                        {BackEndStack.map((tech, index) => (
+                            <ListItem key={index}>
+                                <Box className={styles.parallaxListBox}>
+                                    <ListItemIcon>
+                                        <Box
+                                            component={'img'}
+                                            className={styles.parallaxIcon}
+                                            alt={tech.alt}
+                                            src={tech.img}
+                                        />
+                                    </ListItemIcon>
+                                    <Typography className={styles.parallaxText}>
+                                        {tech.name}
+                                    </Typography>
+                                </Box>
+                            </ListItem>
+                        ))}
+                        <ListItem>
+                            <ListItemButton
+                                className={styles.parallaxButton}
+                                onClick={handleClass2Toggle}
+                                href={'#front-end'}
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <Typography className={styles.buttonText}>
+                                    Learn More
+                                </Typography>
+                                <KeyboardDoubleArrowRightIcon fontSize="large" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </Box>
+                <Box
+                    className={
+                        showClass2
+                            ? styles.parallaxContainer4
+                            : styles.parallaxContainer4 +
+                              ' ' +
+                              styles.showContainer4
+                    }
+                >
+                    <IconButton
+                        onClick={handleClass2Toggle}
+                        href={'#front-end'}
+                    >
+                        <Typography
+                            sx={{ color: 'white', whiteSpace: 'nowrap' }}
+                        >
+                            Close Window
+                        </Typography>
+                        <CloseIcon className={styles.parallaxCloseIcon} />
+                    </IconButton>
+                    <Box
+                        component={'img'}
+                        className={styles.parallaxImg}
+                        alt={FrontEndBox.alt1}
+                        src={FrontEndBox.img1}
+                    />
+                    <Typography className={styles.parallaxDrawerText}>
+                        {FrontEndBox.text1}
+                        <span className={'widow-wrap'}>
+                            {FrontEndBox.text1Widow}
+                        </span>
+                    </Typography>
+                    <Box className={'flex-evenly'}>
+                        <Box
+                            component={'img'}
+                            className={styles.parallaxImg2}
+                            alt={FrontEndBox.alt3}
+                            src={FrontEndBox.img3}
+                        />
+                    </Box>
+                    <Typography className={styles.parallaxDrawerText}>
+                        {FrontEndBox.text2}
+                        <span className={'widow-wrap'}>
+                            {FrontEndBox.text2Widow}
+                        </span>
+                    </Typography>
+                    <Box
+                        component={'img'}
+                        className={styles.parallaxImg}
+                        alt={FrontEndBox.alt2}
+                        src={FrontEndBox.img2}
+                    />
+                    <Typography className={styles.parallaxDrawerText}>
+                        {FrontEndBox.text3}
+                        <span className={'widow-wrap'}>
+                            {FrontEndBox.text3Widow}
+                        </span>
+                    </Typography>
+                    <IconButton
+                        onClick={handleClass2Toggle}
+                        href={'#front-end'}
+                    >
+                        <Typography
+                            sx={{ color: 'white', whiteSpace: 'nowrap' }}
+                        >
+                            Close Window
+                        </Typography>
+                        <CloseIcon
+                            className={styles.parallaxCloseIcon}
+                            href={'#'}
+                        />
+                    </IconButton>
+                </Box>
+            </Box>
         </Box>
     );
 };
--- frontend/src/utils/constants/home-constants.js
+++ frontend/src/utils/constants/home-constants.js
@@ -15,35 +15,30 @@ const FrontEndStack = [
         name: 'HTML5',
         img: '../svg/html-5.svg',
         alt: 'An image of the HTML 5 logo.',
-        url: 'https://html.com/html5/',
     },
     {
         id: 2,
         name: 'CSS',
         img: '../svg/css-3.svg',
         alt: 'An image of the CSS 3 logo.',
-        url: 'https://www.w3.org/Style/CSS/Overview.en.html',
     },
     {
         id: 3,
         name: 'JavaScript',
         img: '../svg/js.svg',
         alt: 'An image of the JavaScript logo.',
-        url: 'https://www.javascript.com/',
     },
     {
         id: 4,
         name: 'React',
         img: '../svg/react.svg',
         alt: 'An image of the React logo.',
-        url: 'https://react.dev/',
     },
     {
         id: 5,
         name: 'Next.js',
         img: '../svg/nextjs.svg',
         alt: 'An image of the Next.js logo.',
-        url: 'https://nextjs.org/',
     },
 ];
 
@@ -78,4 +73,42 @@ const BackEndStack = [
     },
 ];
 
-export { HomeItems, FrontEndStack, BackEndStack };
+const FrontEndBox = {
+    text1: `Single Page Applications using the React framework `,
+    text1Widow: `and Next.js.`,
+    text2: `Fluid and Responsive web design with a `,
+    text2Widow: `mobile-first approach.`,
+    text3: `Experienced with Bootstrap, MUI, and Material Design styling libraries to build clean and `,
+    text3Widow: `user-centric UI/UX.`,
+    img1: `../img/Roles-Landing-Page.webp`,
+    alt1: `An image of a roles-management dashboard with a search bar above and a table of roles below.`,
+    img2: `../img/Roles-ProfileEdit-Page.webp`,
+    alt2: `An image of a roles-management edit page, with a description of the role above, a tab option for 'profile' and for 'permissions', and a form below the tabs to edit the role.`,
+    img3: `../img/SoW-sm.webp`,
+    alt3: `A mobile-view of a web page titled 'The Story Of Water'.`,
+    img4: `../img/SoW-md.webp`,
+    alt4: `A medium-sized view of a web page titled 'The Story Of Water'.`,
+    img5: `../img/SoW-md.webp`,
+    alt5: `A large-sized view of a web page titled 'The Story Of Water'.`,
+};
+
+const BackEndBox = {
+    text1: `Application development in a Microsoft environment with ASP.NET Core. Experience using both MVC razor pages and `,
+    text1Widow: `Web API's.`,
+    text2: `Application development with Java and the Jakarta EE platform. Writing end to end tests using Arquillian and Selenium. Building and consumption of RESTful API's using JAX-RS.`,
+    text2Widow: ``,
+    text3: ``,
+    text3Widow: ``,
+    img1: `../img/Roles-Landing-Page.webp`,
+    alt1: `An image of a roles-management dashboard with a search bar above and a table of roles below.`,
+    img2: `../img/Roles-ProfileEdit-Page.webp`,
+    alt2: `An image of a roles-management edit page, with a description of the role above, a tab option for 'profile' and for 'permissions', and a form below the tabs to edit the role.`,
+    img3: `../img/SoW-sm.webp`,
+    alt3: `A mobile-view of a web page titled 'The Story Of Water'.`,
+    img4: `../img/SoW-md.webp`,
+    alt4: `A medium-sized view of a web page titled 'The Story Of Water'.`,
+    img5: `../img/SoW-md.webp`,
+    alt5: `A large-sized view of a web page titled 'The Story Of Water'.`,
+};
+
+export { HomeItems, FrontEndStack, BackEndStack, FrontEndBox, BackEndBox };
