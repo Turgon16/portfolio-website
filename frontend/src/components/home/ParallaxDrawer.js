8d71aaeba879d7ccae7d09065d73b65d7c5a2b5b
Tyler Hum <tyler.hum@gmail.com>
2023-04-14 18:50:33 -0600

    Styles: Adds centered container and styling

--- frontend/src/components/home/ParallaxDrawer.js
+++ frontend/src/components/home/ParallaxDrawer.js
@@ -4,6 +4,7 @@
 import CloseIcon from '@mui/icons-material/Close';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
 // Project & Style Imports
@@ -41,44 +42,52 @@ const ParallaxDrawer = ({
                       styles.showContainer
             }
         >
-            <CloseWindowButton />
-            <Box className={styles.parallaxDrawerInnerContainer}>
-                <Box
-                    component={'img'}
-                    className={styles.parallaxImg}
-                    alt={techstack.alt1}
-                    src={techstack.img1}
-                />
-                <Typography className={styles.parallaxDrawerText}>
-                    {techstack.text1}
-                    <span className={'widow-wrap'}>{techstack.text1Widow}</span>
-                </Typography>
-            </Box>
-            <Box className={styles.parallaxDrawerInnerContainerMid}>
-                <Box
-                    component={'img'}
-                    className={styles.parallaxImg2}
-                    alt={techstack.alt3}
-                    src={techstack.img3}
-                />
-                <Typography className={styles.parallaxDrawerText}>
-                    {techstack.text2}
-                    <span className={'widow-wrap'}>{techstack.text2Widow}</span>
-                </Typography>
-            </Box>
-            <Box className={styles.parallaxDrawerInnerContainer}>
-                <Box
-                    component={'img'}
-                    className={styles.parallaxImg}
-                    alt={techstack.alt2}
-                    src={techstack.img2}
-                />
-                <Typography className={styles.parallaxDrawerText}>
-                    {techstack.text3}
-                    <span className={'widow-wrap'}>{techstack.text3Widow}</span>
-                </Typography>
-            </Box>
-            <CloseWindowButton />
+            <Container maxWidth="xl">
+                <CloseWindowButton />
+                <Box className={styles.parallaxDrawerInnerContainer}>
+                    <Box
+                        component={'img'}
+                        className={styles.parallaxImg}
+                        alt={techstack.alt1}
+                        src={techstack.img1}
+                    />
+                    <Typography className={styles.parallaxDrawerText}>
+                        {techstack.text1}
+                        <span className={'widow-wrap'}>
+                            {techstack.text1Widow}
+                        </span>
+                    </Typography>
+                </Box>
+                <Box className={styles.parallaxDrawerInnerContainerMid}>
+                    <Box
+                        component={'img'}
+                        className={styles.parallaxImg2}
+                        alt={techstack.alt3}
+                        src={techstack.img3}
+                    />
+                    <Typography className={styles.parallaxDrawerText}>
+                        {techstack.text2}
+                        <span className={'widow-wrap'}>
+                            {techstack.text2Widow}
+                        </span>
+                    </Typography>
+                </Box>
+                <Box className={styles.parallaxDrawerInnerContainer}>
+                    <Box
+                        component={'img'}
+                        className={styles.parallaxImg}
+                        alt={techstack.alt2}
+                        src={techstack.img2}
+                    />
+                    <Typography className={styles.parallaxDrawerText}>
+                        {techstack.text3}
+                        <span className={'widow-wrap'}>
+                            {techstack.text3Widow}
+                        </span>
+                    </Typography>
+                </Box>
+                <CloseWindowButton />
+            </Container>
         </Box>
     );
 };
