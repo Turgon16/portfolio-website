74807b8509a59e1fe13ba2ced771cff8204d2957
Tyler Hum <tyler.hum@gmail.com>
2023-04-14 18:49:59 -0600

    Chore: Removes unused Router import

--- frontend/src/components/home/ParallaxBanner.js
+++ frontend/src/components/home/ParallaxBanner.js
@@ -1,10 +1,10 @@
 // Next & React Imports
-import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 // MUI Imports
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -23,7 +23,6 @@ import ParallaxDrawer from './ParallaxDrawer';
 const ParallaxBanner = () => {
     const [showClass1, setShowClass1] = useState('false');
     const [showClass2, setShowClass2] = useState('false');
-    const router = useRouter();
 
     const handleClass1Toggle = () => {
         setShowClass1(!showClass1);
@@ -35,101 +34,107 @@ const ParallaxBanner = () => {
 
     return (
         <Box id={'front-end'} className={styles.parallaxBanner}>
-            <Box className={styles.parallaxTop}>
-                <Box className={styles.parallaxContainer1}>
-                    <Typography className={styles.parallaxTitle}>
-                        Front-end
-                    </Typography>
-                    <List>
-                        {FrontEndStack.map((tech, index) => (
-                            <ListItem key={index}>
-                                <Box className={styles.parallaxListBox}>
-                                    <ListItemIcon>
-                                        <Box
-                                            component={'img'}
-                                            className={styles.parallaxIcon}
-                                            alt={tech.alt}
-                                            src={tech.img}
-                                        />
-                                    </ListItemIcon>
-                                    <Typography className={styles.parallaxText}>
-                                        {tech.name}
+            <Container className={styles.parallaxCenteredContainer}>
+                <Box className={styles.parallaxTop}>
+                    <Box className={styles.parallaxContainer1}>
+                        <Typography className={styles.parallaxTitle}>
+                            Front-end
+                        </Typography>
+                        <List>
+                            {FrontEndStack.map((tech, index) => (
+                                <ListItem key={index}>
+                                    <Box className={styles.parallaxListBox}>
+                                        <ListItemIcon>
+                                            <Box
+                                                component={'img'}
+                                                className={styles.parallaxIcon}
+                                                alt={tech.alt}
+                                                src={tech.img}
+                                            />
+                                        </ListItemIcon>
+                                        <Typography
+                                            className={styles.parallaxText}
+                                        >
+                                            {tech.name}
+                                        </Typography>
+                                    </Box>
+                                </ListItem>
+                            ))}
+                            <ListItem>
+                                <ListItemButton
+                                    className={styles.parallaxButton}
+                                    onClick={handleClass1Toggle}
+                                    href={'#front-end'}
+                                    sx={{
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <Typography className={styles.buttonText}>
+                                        Learn More
                                     </Typography>
-                                </Box>
+                                    <KeyboardDoubleArrowRightIcon fontSize="large" />
+                                </ListItemButton>
                             </ListItem>
-                        ))}
-                        <ListItem>
-                            <ListItemButton
-                                className={styles.parallaxButton}
-                                onClick={handleClass1Toggle}
-                                href={'#front-end'}
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                }}
-                            >
-                                <Typography className={styles.buttonText}>
-                                    Learn More
-                                </Typography>
-                                <KeyboardDoubleArrowRightIcon fontSize="large" />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
+                        </List>
+                    </Box>
+                    <ParallaxDrawer
+                        handleClass1Toggle={handleClass1Toggle}
+                        showClassFunction={showClass1}
+                        techstack={FrontEndBox}
+                    />
                 </Box>
-                <ParallaxDrawer
-                    handleClass1Toggle={handleClass1Toggle}
-                    showClassFunction={showClass1}
-                    techstack={FrontEndBox}
-                />
-            </Box>
 
-            <Box className={styles.parallaxBottom}>
-                <Box className={styles.parallaxContainer2}>
-                    <Typography className={styles.parallaxTitle}>
-                        Back-end
-                    </Typography>
-                    <List>
-                        {BackEndStack.map((tech, index) => (
-                            <ListItem key={index}>
-                                <Box className={styles.parallaxListBox}>
-                                    <ListItemIcon>
-                                        <Box
-                                            component={'img'}
-                                            className={styles.parallaxIcon}
-                                            alt={tech.alt}
-                                            src={tech.img}
-                                        />
-                                    </ListItemIcon>
-                                    <Typography className={styles.parallaxText}>
-                                        {tech.name}
+                <Box className={styles.parallaxBottom}>
+                    <Box className={styles.parallaxContainer2}>
+                        <Typography className={styles.parallaxTitle}>
+                            Back-end
+                        </Typography>
+                        <List>
+                            {BackEndStack.map((tech, index) => (
+                                <ListItem key={index}>
+                                    <Box className={styles.parallaxListBox}>
+                                        <ListItemIcon>
+                                            <Box
+                                                component={'img'}
+                                                className={styles.parallaxIcon}
+                                                alt={tech.alt}
+                                                src={tech.img}
+                                            />
+                                        </ListItemIcon>
+                                        <Typography
+                                            className={styles.parallaxText}
+                                        >
+                                            {tech.name}
+                                        </Typography>
+                                    </Box>
+                                </ListItem>
+                            ))}
+                            <ListItem>
+                                <ListItemButton
+                                    className={styles.parallaxButton}
+                                    onClick={handleClass2Toggle}
+                                    href={'#front-end'}
+                                    sx={{
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <Typography className={styles.buttonText}>
+                                        Learn More
                                     </Typography>
-                                </Box>
+                                    <KeyboardDoubleArrowRightIcon fontSize="large" />
+                                </ListItemButton>
                             </ListItem>
-                        ))}
-                        <ListItem>
-                            <ListItemButton
-                                className={styles.parallaxButton}
-                                onClick={handleClass2Toggle}
-                                href={'#front-end'}
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                }}
-                            >
-                                <Typography className={styles.buttonText}>
-                                    Learn More
-                                </Typography>
-                                <KeyboardDoubleArrowRightIcon fontSize="large" />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
+                        </List>
+                    </Box>
+                    <ParallaxDrawer
+                        handleClass1Toggle={handleClass2Toggle}
+                        showClassFunction={showClass2}
+                        techstack={BackEndBox}
+                    />
                 </Box>
-                <ParallaxDrawer
-                    handleClass1Toggle={handleClass2Toggle}
-                    showClassFunction={showClass2}
-                    techstack={BackEndBox}
-                />
-            </Box>
+            </Container>
         </Box>
     );
 };
