82b881553033d559c426db0d5eb171e790aaa3d2
Tyler Hum <tyler.hum@gmail.com>
2023-04-28 14:52:11 -0600

    Refactor: removes old code

--- frontend/src/components/home/CardsBanner.js
+++ frontend/src/components/home/CardsBanner.js
@@ -42,31 +42,6 @@ const CardsBanner = () => {
                         </CardContent>
                     </Card>
                 ))}
-                {/* <Card className={styles.card}>
-                    <CardContent className={styles.cardContainer}>
-                        <TipsAndUpdatesIcon className={styles.cardsIcon} />
-                        <Typography>Design</Typography>
-                    </CardContent>
-                </Card>
-                <Card className={styles.card}>
-                    <CardContent className={styles.cardContainer}>
-                        <Box
-                            component={'img'}
-                            className={styles.cardsIcon}
-                            alt={
-                                'An icon representing the scrum arrows, which loop in a circle and then back out again.'
-                            }
-                            src={'../svg/scrum.svg'}
-                        />
-                        <Typography>Agile Approach</Typography>
-                    </CardContent>
-                </Card>
-                <Card className={styles.card}>
-                    <CardContent className={styles.cardContainer}>
-                        <ArticleIcon className={styles.cardsIcon} />
-                        <Typography>Documentation</Typography>
-                    </CardContent>
-                </Card> */}
             </Container>
         </Box>
     );
